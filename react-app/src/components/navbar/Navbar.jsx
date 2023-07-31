49825494010191cff885164370c70f67af3f2ec7
John Mitchell <jhmitchell@uchicago.edu>
2023-07-30 19:09:12 -0500

    nav-container

--- react-app/src/components/navbar/Navbar.jsx
+++ react-app/src/components/navbar/Navbar.jsx
@@ -5,12 +5,16 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navTitle">
-        <p>CORNWALL</p>
-      </div>
-      <div className="navLinks">
-        <Link to="/">Home</Link>
-        <Link to="/">About</Link>
+      <div className="nav-container">
+        <div className="nav-title">
+          <p>CORNWALL</p>
+        </div>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/">About</Link>
+        </div>
+        <div className="nav-profile">
+        </div>
       </div>
     </nav>
   );
