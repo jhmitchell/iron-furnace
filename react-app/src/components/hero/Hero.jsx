de1845d4b95751b42f56b4e8a58217f32118d345
John Mitchell <jhmitchell@uchicago.edu>
2023-08-02 21:41:54 -0500

    Reworked hero with filtering not applied to text

--- react-app/src/components/hero/Hero.jsx
+++ react-app/src/components/hero/Hero.jsx
@@ -4,6 +4,7 @@ import "./Hero.css";
 const Hero = () => {
   return (
     <div className="hero">
+      <div className="hero-image"></div>
       <div className="hero-text">
         <h1>CORNWALL</h1>
         <h2>EXPLORING AMERICA'S RICH INDUSTRIAL HERITAGE</h2>
