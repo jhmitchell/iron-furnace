1f82c84f65d5a23480a3b1b3cbc99d15068abe57
John Mitchell <jhmitchell@uchicago.edu>
2023-08-22 00:36:25 -0500

    become member styling

--- react-app/src/layouts/navbar/membershipActions/MembershipActions.jsx
+++ react-app/src/layouts/navbar/membershipActions/MembershipActions.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useAuth } from "../../../features/authentication";
 import { Link } from "react-router-dom";
+import { Button } from "../../../components/ui";
+import "./MembershipActions.css";
 
 const MembershipActions = () => {
   // Import the user object from the useAuth hook
@@ -11,9 +13,9 @@ const MembershipActions = () => {
       {user ? (
         <Link to="/profile">Welcome, {user.username}</Link>
       ) : (
-        <span>
+        <span className="membership-links">
           <Link to="/membership">SIGN IN</Link>
-          <Link to="/membership">BECOME A MEMBER</Link>
+          <Link to="/membership"><Button text="BECOME A MEMBER" color="orange"/></Link>
         </span>
       )}
     </div>
