347eac5a215cca4a916fe2b3d8521d88910e20a8
John Mitchell <jhmitchell@uchicago.edu>
2023-08-24 00:24:31 -0500

    temp events card section

--- react-app/src/features/home/components/eventsSection/EventsSection.jsx
+++ react-app/src/features/home/components/eventsSection/EventsSection.jsx
@@ -5,28 +5,31 @@ import "./EventsSection.css";
 const EventsSection = () => {
   return (
     <section className="events-section">
+    <div className="content-container"> {/* Added content container */}
+      <h2 className="section-title">Upcoming Events</h2>
       <div className="events-flex">
         <EventCard
-          image="/src/assets/images/big_wheel.jpg"
+          image="/src/assets/images/events/jim2023.jpg"
           title="Industrial Innovation Conference"
           date="September 10, 2023"
           description="Join us for a day of insights into the world of industrial innovation."
           path="#"
         />
         <EventCard
-          image="/src/assets/images/entrance.jpg"
+          image="/src/assets/images/events/patrick2023.jpg"
           title="Cornwall Heritage Festival"
           date="October 5, 2023"
           description="Celebrate the rich heritage of Cornwall with food, music, and art."
           path="#"
         />
         <EventCard
-          image="/src/assets/images/hero_demo.png"
+          image="/src/assets/images/events/mike2023.jpg"
           title="Sustainable Engineering Workshop"
           date="November 18, 2023"
           description="A hands-on workshop focused on sustainable practices in modern engineering."
           path="#"
         />
+        </div>
       </div>
     </section>
   );
