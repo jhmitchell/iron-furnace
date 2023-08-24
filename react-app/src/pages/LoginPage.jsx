4ff6ff0cae1dc1dfc7afc653f64e9e3b3e5cc097
John Mitchell <jhmitchell@uchicago.edu>
2023-08-24 00:44:38 -0500

    start login page

--- react-app/src/pages/LoginPage.jsx
+++ react-app/src/pages/LoginPage.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import { Hero, WelcomeFlex } from "../features/home";
-import BlankSection from "../components/blankSection/BlankSection";
-import MainLayout from "../layouts/MainLayout";
 import "./LoginPage.css";
 
 const LoginPage = () => {
