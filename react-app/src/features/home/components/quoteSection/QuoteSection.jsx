493c9972baebbb4ee3b66f8aa2b7a975fc8534d7
John Mitchell <jhmitchell@uchicago.edu>
2023-09-07 18:23:41 -0500

    improved quote

--- react-app/src/features/home/components/quoteSection/QuoteSection.jsx
+++ react-app/src/features/home/components/quoteSection/QuoteSection.jsx
@@ -6,12 +6,9 @@ const QuoteSection = () => {
         <section className="quote-section">
             <div className="quote-container">
                 <div className="quote">
-                    <p className="quote-text">
-                        A site of "transcendent significance"
-                    </p>
-                    <p className="below-text">
-                        In these words, Robert Vogel described the Cornwall Iron Furnace...
-                    </p>
+                    <p className="quote-text">A Site of Transcendent Significance.</p>
+                    <p className="quote-continuation">With the exception of a mere handful of similar preservations in Sweden and Germany - and possibly a few in eastern Europe - I doubt that elsewhere in the world is there a 19th-century iron furnace complex with the degree of historical integrity to be found at Cornwall…</p>
+                    <p className="quote-author">- Robert Vogel, Smithsonian Institution</p>
                 </div>
             </div>
         </section>
