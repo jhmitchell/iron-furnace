08863ebfd857071582fb47d11ddff6f1fdebf56f
John Mitchell <jhmitchell@uchicago.edu>
2023-09-12 18:01:36 -0500

    Improved ArrowTextLink with size prop, enhanced 404 page

--- react-app/src/components/ui/arrowTextLink/ArrowTextLink.jsx
+++ react-app/src/components/ui/arrowTextLink/ArrowTextLink.jsx
@@ -2,14 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './ArrowTextLink.css';
 
-const ArrowTextLink = ({ text, path }) => (
-  <Link to={path} className="animated-arrow">
-    <span className="the-arrow -left"><span className="shaft"></span></span>
-    <span className="arrow-main">
-      <span className="text">{text}</span>
-      <span className="the-arrow -right"><span className="shaft"></span></span>
-    </span>
-  </Link>
-);
+const ArrowTextLink = ({ text, path, fontSize="1.5" }) => {
+  const styleVariables = {
+    '--fontSize': `${fontSize}rem`,
+    '--text-arrow-space': `${fontSize * 0.5}rem`,
+    '--shaft-width': `${fontSize * 1.7}rem`,
+    '--shaft-thickness': `${fontSize * 0.1}rem`,
+    '--arrow-head-width': `${fontSize * 0.5}rem`,
+    '--arrow-head-thickness': `${fontSize * 0.1}rem`
+  };
+
+  return (
+    <Link to={path} className="animated-arrow" style={styleVariables}>
+      <span className="the-arrow -left"><span className="shaft"></span></span>
+      <span className="arrow-main">
+        <span className="text">{text}</span>
+        <span className="the-arrow -right"><span className="shaft"></span></span>
+      </span>
+    </Link>
+  );
+};
 
 export default ArrowTextLink;
--- react-app/src/pages/NotFound.jsx
+++ react-app/src/pages/NotFound.jsx
@@ -1,12 +1,16 @@
-import React from "react";
-import "./NotFound.css";
+import React from 'react';
+import { ArrowTextLink } from '../components/ui';
+import './NotFound.css';
 
 const NotFound = () => {
   return (
     <div className="not-found-page">
       <div className="text-flex">
-        <h1>404</h1>
-        <h2>Page Not Found</h2>
+        <h1>:-(</h1>
+        <h2>We couldn't find the page you're looking for.</h2>
+        <p>Want to go back?
+          <ArrowTextLink text="TAKE ME HOME" path="/" fontSize="1.5"/>
+        </p>
       </div>
     </div>
   );
