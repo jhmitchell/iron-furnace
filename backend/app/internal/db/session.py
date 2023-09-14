95d18753ead5d72239268bde91140a52248c9919
John Mitchell <jhmitchell@uchicago.edu>
2023-09-14 16:19:25 -0500

    add root user

--- backend/app/internal/db/session.py
+++ backend/app/internal/db/session.py
@@ -3,8 +3,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.internal.models import Base
+from dotenv import load_dotenv
 import os
 
+# Load variables from .env
+load_dotenv("app/.env")
 DATABASE_URL = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
 
 engine = create_engine(DATABASE_URL)
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,17 @@
 import os
 from dotenv import load_dotenv
+import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from .routers import authentication, users, hours
+from .internal.token import hash_password
+from .internal.db.session import get_db
+from .internal.db.users import create_user
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Load the .env file
 load_dotenv()
@@ -28,3 +37,33 @@ app.add_middleware(
 app.include_router(authentication.router, prefix=f'{API_V1_PREFIX}{AUTH_PREFIX}')
 app.include_router(users.router, prefix=f'{API_V1_PREFIX}')
 app.include_router(hours.router, prefix=f'{API_V1_PREFIX}')
+
+
+@app.on_event("startup")
+async def startup_event():
+    logger.info("Initializing server...")
+    # Get root user information from environment variables
+    root_username = os.getenv("ROOT_USERNAME")
+    root_first_name = os.getenv("ROOT_FIRST_NAME")
+    root_last_name = os.getenv("ROOT_LAST_NAME")
+    root_email = os.getenv("ROOT_EMAIL")
+    root_password = os.getenv("ROOT_PASSWORD")
+
+    hashed_password = hash_password(root_password)
+
+    db = next(get_db())
+    result = create_user(
+        db,
+        root_username,
+        root_email,
+        hashed_password,
+        root_first_name,
+        root_last_name,
+        True
+    )
+
+    # Check the result and print the status
+    if result['status'] == 'success':
+        logger.info(f'Created root user {root_username}')
+    
+    logger.info(f'Initialized server in {env} mode')
