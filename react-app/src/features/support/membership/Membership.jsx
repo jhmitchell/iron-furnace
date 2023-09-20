f09a8ad0178ce7280191f3255ace023bb293a718
John Mitchell <jhmitchell@uchicago.edu>
2023-09-19 19:39:41 -0500

    membership tiers

--- react-app/src/features/support/membership/Membership.jsx
+++ react-app/src/features/support/membership/Membership.jsx
@@ -5,7 +5,21 @@ const Membership = () => {
   return (
     <div className="membership-section">
       <h2>Why Become a Member?</h2>
-      <p>Benefits and perks...</p>
+      <p>Discover the benefits and perks of different membership tiers.</p>
+      <div className="tiers">
+        <div className="tier">
+          <h3>Basic</h3>
+          <p>Some description...</p>
+        </div>
+        <div className="tier">
+          <h3>Pro</h3>
+          <p>Some description...</p>
+        </div>
+        <div className="tier">
+          <h3>Elite</h3>
+          <p>Some description...</p>
+        </div>
+      </div>
     </div>
   );
 };
