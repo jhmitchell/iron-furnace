5c4d96fd596462121cd6142f0633804faa13f346
John Mitchell <jhmitchell@uchicago.edu>
2023-09-26 17:56:02 -0500

    join/renew card

--- react-app/src/features/support/membership/Membership.jsx
+++ react-app/src/features/support/membership/Membership.jsx
@@ -3,7 +3,8 @@ import { Button } from "/src/components/ui";
 import "./Membership.css";
 
 /**
- * The Membership component renders the membership section, comprising of a description area and a link to the detailed membership page.
+ * The Membership component renders the membership section, comprising of a 
+ * description area and a link to the detailed membership page.
  *
  * @returns {React.Element} JSX element
  */
@@ -16,15 +17,27 @@ const Membership = () => {
           <p className="friend-header">As a friend of Cornwall Iron Furnace…</p>
           <div className="text-content">
             <ul>
-              <li>You are preserving our iconic and historic site for future generations.</li>
-              <li>You are ensuring that Cornwall Iron Furnace remains accessible to visitors and students each year through educational programming and admission.</li>
-              <li>You are helping to tell our century-old history to thousands of visitors each year.</li>
+              <li>
+                You are preserving our iconic and historic site for future
+                generations.
+              </li>
+              <li>
+                You are ensuring that Cornwall Iron Furnace remains accessible
+                to visitors and students each year through educational
+                programming and admission.
+              </li>
+              <li>
+                You are helping to tell our century-old history to thousands of
+                visitors each year.
+              </li>
             </ul>
           </div>
         </div>
         <div className="right-side-content">
-          <p className="signin-text">Interested in becoming a member?</p>
-          <Button color="orange" size="medium" text="JOIN/RENEW NOW"/>
+        <div className="membership-card">
+            <p className="signin-text">Interested in becoming a member?</p>
+            <Button color="orange" size="medium" text="JOIN/RENEW NOW" />
+          </div>
         </div>
       </div>
     </div>
