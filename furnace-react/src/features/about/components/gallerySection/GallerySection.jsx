4c2dedf213ddf10c96afa15d0a4d02419095330e
jhmitchell <mitchdog99@gmail.com>
2024-02-20 15:49:35 -0600

    init photo gallery

--- furnace-react/src/features/about/components/gallerySection/GallerySection.jsx
+++ furnace-react/src/features/about/components/gallerySection/GallerySection.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import PhotoAlbum from "react-photo-album";
 import ScrollableSection from "/src/layouts/scrollableSection/ScrollableSection";
 
+const photos = [
+	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
+	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
+	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
+	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
+	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
+	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
+	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
+	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
+	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
+	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
+];
 
 const GallerySection = () => {
 	return (
 		<ScrollableSection id="gallery-section" title="Gallery">
-			Gallery here
+			<PhotoAlbum layout="rows" photos={photos} />
 		</ScrollableSection>
 	);
 };
