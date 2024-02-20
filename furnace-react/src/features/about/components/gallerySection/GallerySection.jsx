85d15be94f4a8668264622cadde59f36c538b33a
jhmitchell <mitchdog99@gmail.com>
2024-02-20 16:03:57 -0600

    images as modules

--- furnace-react/src/features/about/components/gallerySection/GallerySection.jsx
+++ furnace-react/src/features/about/components/gallerySection/GallerySection.jsx
@@ -2,17 +2,23 @@ import React from "react";
 import PhotoAlbum from "react-photo-album";
 import ScrollableSection from "/src/layouts/scrollableSection/ScrollableSection";
 
+import FurnaceImg from "/src/assets/images/furnace-top.webp";
+
 const photos = [
-	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
-	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
-	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
-	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
-	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
-	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
-	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
-	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
-	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
-	{ src: "/src/assets/images/furnace-top.webp", width: 800, height: 600 },
+	{ src: FurnaceImg, width: 800, height: 600 },
+	{ src: FurnaceImg, width: 800, height: 600 },
+	{ src: FurnaceImg, width: 800, height: 600 },
+	{ src: FurnaceImg, width: 800, height: 600 },
+	{ src: FurnaceImg, width: 800, height: 600 },
+	{ src: FurnaceImg, width: 800, height: 600 },
+	{ src: FurnaceImg, width: 800, height: 600 },
+	{ src: FurnaceImg, width: 800, height: 600 },
+	{ src: FurnaceImg, width: 800, height: 600 },
+	{ src: FurnaceImg, width: 800, height: 600 },
+	{ src: FurnaceImg, width: 800, height: 600 },
+	{ src: FurnaceImg, width: 800, height: 600 },
+	{ src: FurnaceImg, width: 800, height: 600 },
+	{ src: FurnaceImg, width: 800, height: 600 },
 ];
 
 const GallerySection = () => {
