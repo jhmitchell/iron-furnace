5c2f4e81371939b3ded3fd0cd41f53fcd8f5b78f
jhmitchell <mitchdog99@gmail.com>
2024-03-27 14:22:10 -0500

    server startup creates db tables if not found, added events table

---
+++ backend/app/internal/db/init.py
@@ -0,0 +1,101 @@
+import os
+import logging
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+from app.internal.db.users import create_user
+from app.internal.token import hash_password
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def check_and_create_table(db, query: str, table_name: str):
+    query_check = f"SHOW TABLES LIKE '{table_name}';"
+    result = db.execute(text(query_check))
+    table_exists = result.fetchone()
+
+    if not table_exists:
+        db.execute(text(query))
+        db.commit()
+        logger.info(f"Table {table_name} created.")
+    else:
+        logger.info(f"Table {table_name} found.")
+
+
+def create_users_table(db: Session):
+    query = """
+    CREATE TABLE IF NOT EXISTS users (
+        member_id VARCHAR(255) NOT NULL PRIMARY KEY,
+        email VARCHAR(255) UNIQUE,
+        hashed_password VARCHAR(255),
+        first_name VARCHAR(255),
+        last_name VARCHAR(255),
+        disabled TINYINT(1) DEFAULT 0,
+        refresh_token VARCHAR(255),
+        refresh_token_expires_at DATETIME
+    );
+    """
+    check_and_create_table(db, query, "users")
+
+
+def create_hours_table(db: Session):
+    query = """
+    CREATE TABLE IF NOT EXISTS operating_hours (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        day_of_week ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'),
+        open_time TIME,
+        close_time TIME
+    );
+    """
+    check_and_create_table(db, query, "operating_hours")
+
+
+def create_holidays_table(db: Session):
+    query = """
+    CREATE TABLE IF NOT EXISTS holidays (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        holiday_date DATE,
+        description VARCHAR(255)
+    );
+    """
+    check_and_create_table(db, query, "holidays")
+
+
+def create_events_table(db: Session):
+    query = """
+    CREATE TABLE IF NOT EXISTS events (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        event_start DATETIME NOT NULL,
+        image VARCHAR(255),
+        title VARCHAR(255) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        description TEXT,
+        link_text VARCHAR(255),
+        link_url VARCHAR(255)
+    );
+    """
+    check_and_create_table(db, query, "events")
+
+
+def create_root_user(db: Session):
+    # Get root user information from environment variables
+    root_username = os.getenv("ROOT_USERNAME")
+    root_first_name = os.getenv("ROOT_FIRST_NAME")
+    root_last_name = os.getenv("ROOT_LAST_NAME")
+    root_email = os.getenv("ROOT_EMAIL")
+    root_password = os.getenv("ROOT_PASSWORD")
+
+    hashed_password = hash_password(root_password)
+
+    result = create_user(
+        db,
+        root_username,
+        root_email,
+        hashed_password,
+        root_first_name,
+        root_last_name,
+        True
+    )
+
+    if result['status'] == 'success':
+        logger.info(f'Created root user {root_username}')
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,11 +3,15 @@ from dotenv import load_dotenv
 import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import text
 
 from .routers import authentication, users, hours
-from .internal.token import hash_password
 from .internal.db.session import get_db
-from .internal.db.users import create_user
+from .internal.db.init import (create_users_table, 
+                               create_hours_table, 
+                               create_holidays_table, 
+                               create_events_table, 
+                               create_root_user)
 
 # Initialize logging
 logging.basicConfig(level=logging.INFO)
@@ -25,7 +29,8 @@ AUTH_PREFIX = os.getenv("AUTH_PREFIX")
 app = FastAPI()
 
 # Define CORS settings based on the environment
-origins = [client_url] if client_url else []  # Use an empty list if CLIENT_URL is not defined
+# Use an empty list if CLIENT_URL is not defined
+origins = [client_url] if client_url else []
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -34,7 +39,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(authentication.router, prefix=f'{API_V1_PREFIX}{AUTH_PREFIX}')
+app.include_router(authentication.router,
+                   prefix=f'{API_V1_PREFIX}{AUTH_PREFIX}')
 app.include_router(users.router, prefix=f'{API_V1_PREFIX}')
 app.include_router(hours.router, prefix=f'{API_V1_PREFIX}')
 
@@ -42,28 +48,16 @@ app.include_router(hours.router, prefix=f'{API_V1_PREFIX}')
 @app.on_event("startup")
 async def startup_event():
     logger.info("Initializing server...")
-    # Get root user information from environment variables
-    root_username = os.getenv("ROOT_USERNAME")
-    root_first_name = os.getenv("ROOT_FIRST_NAME")
-    root_last_name = os.getenv("ROOT_LAST_NAME")
-    root_email = os.getenv("ROOT_EMAIL")
-    root_password = os.getenv("ROOT_PASSWORD")
 
-    hashed_password = hash_password(root_password)
-
+    # Create tables and root user if they do not exist
     db = next(get_db())
-    result = create_user(
-        db,
-        root_username,
-        root_email,
-        hashed_password,
-        root_first_name,
-        root_last_name,
-        True
-    )
+    try:
+        create_users_table(db)
+        create_hours_table(db)
+        create_holidays_table(db)
+        create_events_table(db)
+        create_root_user(db)
+    finally:
+        db.close()
 
-    # Check the result and print the status
-    if result['status'] == 'success':
-        logger.info(f'Created root user {root_username}')
-    
     logger.info(f'Initialized server in {env} mode')
