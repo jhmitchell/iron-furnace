6186416311adef67ac7d908e0685cd510e334dd3
jhmitchell <mitchdog99@gmail.com>
2024-03-29 19:34:28 -0500

    remote path debug

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,6 +52,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.on_event("startup")
 async def startup_event():
     logger.info("Initializing server...")
+    logger.info("Current directory: " + os.getcwd())
 
     # Create tables and root user if they do not exist
     db = next(get_db())
