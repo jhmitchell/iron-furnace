776f23d86e3a9cbde154449f4abe543a6ddb06e8
jhmitchell <mitchdog99@gmail.com>
2024-03-29 20:11:37 -0500

    vite-config

--- furnace-react/vite.config.js
+++ furnace-react/vite.config.js
@@ -1,16 +1,13 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-const API_PREFIX = '/api/v1'
-const STATIC_DIR = '/static'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      [API_PREFIX]: 'http://localhost:3001',
-      [STATIC_DIR]: 'http://localhost:3001',
+      '/api/v1': 'http://localhost:3001',
+      '/static': 'http://localhost:3001',
     },
     port: 3000,
     host: true,
