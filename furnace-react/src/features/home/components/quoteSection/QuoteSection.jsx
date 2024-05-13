2e47a5357cf59a0486afdc3a09a0350d789c05f4
jhmitchell <mitchdog99@gmail.com>
2024-05-12 20:04:24 -0500

    parallax quote section

--- furnace-react/src/features/home/components/quoteSection/QuoteSection.jsx
+++ furnace-react/src/features/home/components/quoteSection/QuoteSection.jsx
@@ -1,18 +1,23 @@
+// QuoteSection.js
 import React from "react";
-import "./QuoteSection.css";
+import styles from "./QuoteSection.module.css";
+import QuoteImage from "/src/assets/images/entrance.webp";
 
 const QuoteSection = () => {
-    return (
-        <section className="quote-section">
-            <div className="quote-container">
-                <div className="quote">
-                    <p className="quote-text">A Site of Transcendent Significance.</p>
-                    <p className="quote-continuation">With the exception of a mere handful of similar preservations in Sweden and Germany - and possibly a few in eastern Europe - I doubt that elsewhere in the world is there a 19th-century iron furnace complex with the degree of historical integrity to be found at Cornwall…</p>
-                    <p className="quote-author">- Robert Vogel, Smithsonian Institution</p>
-                </div>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section className={styles.quoteSection} style={{ backgroundImage: `url(${QuoteImage})` }}>
+      <div className={styles.quoteOverlay}></div>
+      <div className={styles.quoteContainer}>
+        <div className={styles.quote}>
+          <p className={styles.quoteText}>A Site of Transcendent Significance.</p>
+          <p className={styles.quoteContinuation}>
+            With the exception of a mere handful of similar preservations in Sweden and Germany - and possibly a few in eastern Europe - I doubt that elsewhere in the world is there a 19th-century iron furnace complex with the degree of historical integrity to be found at Cornwall…
+          </p>
+          <p className={styles.quoteAuthor}>- Robert Vogel, Smithsonian Institution</p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default QuoteSection;
--- furnace-react/src/features/home/components/welcomeFlex/WelcomeFlex.jsx
+++ furnace-react/src/features/home/components/welcomeFlex/WelcomeFlex.jsx
@@ -1,30 +1,17 @@
 import React from "react";
-import { Button } from "../../../../components/ui";
-import { ArrowTextLink } from "../../../../components/ui";
+import { Button, ArrowTextLink } from "/src/components/ui";
 import Hours from "../hours/Hours";
-import "./WelcomeFlex.css";
+import styles from "./WelcomeFlex.module.css";
 
 const WelcomeFlex = () => {
   return (
-    <div className="welcome-flex-container">
-      <div className="welcome-flex">
-        <div className="welcome-flex-element welcome">
-          {/*<h2>Welcome</h2>*/}
+    <div className={styles.welcomeFlexContainer}>
+      <div className={styles.welcomeFlex}>
+        <div className={`${styles.welcomeFlexElement} ${styles.welcome}`}>
           <h3>
-            Welcome to the historic Cornwall Iron Furnace - the most intact and complete
-            charcoal-fueled ironmaking complex found in America.
+            Welcome to the historic Cornwall Iron Furnace - the most intact and complete charcoal-fueled ironmaking complex found in America.
             <ArrowTextLink text="LEARN MORE" path="/about" />
           </h3>
-          {/*
-          <div className="cta-buttons">
-            <Button text="PLAN YOUR VISIT" color="orange" onClick={onClickCTA} />
-            <Button
-              text="LEARN MORE"
-              color="transparent"
-              onClick={onClickCTA}
-            />
-          </div>
-        */}
         </div>
         <Hours />
       </div>
